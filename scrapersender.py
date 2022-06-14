1cebbcabcc8b8db5224d3b013d7c2871f1460c55
Astoshan Stuerm <astoshanstuerm1@gmail.com>
2022-06-14 14:45:09 -0600

    finished basic email function. Additionally I am restructurin the main file format.

--- scrapersender.py
+++ scrapersender.py
@@ -1,42 +1,84 @@
+
+# # find websites to scrape emails from
+# website_list = [""]
+
+# #Configure User Options
+# online = input("Do you want to scrape websites or use a premade .csv file?(enter scrape to scrape or premade for premade file.)  ")
+# # app_code = input("input your application code provided by your google account: ")
+# # email_address = input("input the email you would like to use(note: This email sender is autoconfigured for gmail if you need to use a different server change the piece of code bellow): ")
+
+# if online == "scrape":
+# 	print("[SCRAPING]")
+# elif online == "premade":
+# 	file_name = input("What is the name of the file youw ould like to use?")
+# 	with open(file_name, 'r') as csv_file:
+# 		csv_reader = csv.reader(csv_file)
+# else:
+# 	print("[ERROR:] Invalid option please input one of the options specified in initial question.")
+
+
+
+# # Send emails from the csv file
+# import smtplib
+
+app_code = "youdon'tgetmycode"#This is only for testing purposes the commented input is what you will use above
+email_address = "thisemailisdefinetlynotanad@gmail.com"
+
+# with open("base.csv", 'r') as csv_file:
+# 	csv_reader = csv.reader(csv_file)
+
+# for line in csv_reader:
+# 	with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+# 		smtp.ehlo()
+# 		smtp.starttls()
+# 		smtp.ehlo()
+
+# 		smtp.login(email_address, app_code)
+
+# 		subject = str(input("What do you want the subject of the email to be?  "))
+# 		body = str(input("What do you want the body of the email to be?  "))
+
+# 		msg = f'Subject: {subject}\n\n{body}'
+
+# 		smtp.sendmail(email_address, "astoshanstuerm1@gmail.com", msg)
+
+
 import requests
-import yagmail
-
-
-# find websites to scrape emails from
-website_list = [""]
-
-#Configure User Options
-online = input("Do you want to scrape websites or use a premade .csv file?(enter scrape to scrape or premade for premade file.)  ")
-
-if online == "scrape":
-	print("[SCRAPING]")
-elif online == "premade":
-	print("input file name")
-
-else:
-	print("[ERROR:] Invalid option please input one of the options specified in initial question.")
+import csv
 
 
 
-# Send emails from the csv file
-
-yagmail.register('thisemailisdefinetlynotanad@gmail.com', 'IloveAsuseventhoughitsucks')
-
-reciever = "astoshanstuerm1@gmail.com"
-body = "Hello there from yagmail"
+#Scraping Function
 
 
-yag = yagmail.SMTP("thisemailisdefinetlynotanad@gmail.com")
-yag.send(
-	to=reciever,
-	subject="Yagmail test with attachement",
-	contents=body
-)
+#Email Function
+import smtplib
+def email_sender():
+	file_name = input("What is the name of the file youw ould like to use?")
+	subject = str(input("What do you want the subject of the email to be?  "))
+	body = str(input("What do you want the body of the email to be?  "))
+	with open(file_name, 'r') as csv_file:
+		csv_reader = csv.reader(csv_file)
+
+		for line in csv_reader:
+			with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+				smtp.ehlo()
+				smtp.starttls()
+				smtp.ehlo()
+
+				smtp.login(email_address, app_code)
+
+				subject = subject
+				body = body
+
+				msg = f'Subject: {subject}\n\n{body}'
+
+				smtp.sendmail(email_address, "astoshanstuerm1@gmail.com", msg)
 
 
 
 
+#User_Options Config
 
-
-
+email_sender()
 
