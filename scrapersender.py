a33fd99f62847f3570ccca7535d97ea380ee9ba9
Astoshan Stuerm <astoshanstuerm1@gmail.com>
2022-06-14 16:27:55 -0600

    Added the scraping feature, finished configuring user-options, and restructured main file. All in all I finished the program.

--- scrapersender.py
+++ scrapersender.py
@@ -1,62 +1,46 @@
-
-# # find websites to scrape emails from
-# website_list = [""]
-
-# #Configure User Options
-# online = input("Do you want to scrape websites or use a premade .csv file?(enter scrape to scrape or premade for premade file.)  ")
-# # app_code = input("input your application code provided by your google account: ")
-# # email_address = input("input the email you would like to use(note: This email sender is autoconfigured for gmail if you need to use a different server change the piece of code bellow): ")
-
-# if online == "scrape":
-# 	print("[SCRAPING]")
-# elif online == "premade":
-# 	file_name = input("What is the name of the file youw ould like to use?")
-# 	with open(file_name, 'r') as csv_file:
-# 		csv_reader = csv.reader(csv_file)
-# else:
-# 	print("[ERROR:] Invalid option please input one of the options specified in initial question.")
-
-
-
-# # Send emails from the csv file
-# import smtplib
-
-app_code = "youdon'tgetmycode"#This is only for testing purposes the commented input is what you will use above
-email_address = "thisemailisdefinetlynotanad@gmail.com"
-
-# with open("base.csv", 'r') as csv_file:
-# 	csv_reader = csv.reader(csv_file)
-
-# for line in csv_reader:
-# 	with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-# 		smtp.ehlo()
-# 		smtp.starttls()
-# 		smtp.ehlo()
-
-# 		smtp.login(email_address, app_code)
-
-# 		subject = str(input("What do you want the subject of the email to be?  "))
-# 		body = str(input("What do you want the body of the email to be?  "))
-
-# 		msg = f'Subject: {subject}\n\n{body}'
-
-# 		smtp.sendmail(email_address, "astoshanstuerm1@gmail.com", msg)
-
-
 import requests
+import re
+from requests_html import HTMLSession
 import csv
+import smtplib
 
-
+#Default Messages
+error = "[ERROR] invalid option please use one of the options specified above"
+done = "[DONE] Program finished running. All processes finished successfully"
 
 #Scraping Function
+def email_scraper():
+	url = input("This only works for one url at a time. Input a url here: ")
+	EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+	session = HTMLSession()
+	r = session.get(url)	
+	java = input("Does the website store emails in javascript? ")
+	if java == "yes":
+		r.html.render()
+	elif java == "no":
+		pass
+	else:
+		print(error)
+
+	csv_file_name = input("What is the name of the csv file that you would like to save to? ")
+	email_together = []
+	with open(csv_file_name, 'w', newline = '') as new_file:
+		csv_writer = csv.writer(new_file)
+		for re_match in re.finditer(EMAIL_REGEX, r.html.raw_html.decode()):
+
+			email = re_match.group()
+			email_together.append(email)
+		csv_writer.writerow(email_together)
+
 
 
 #Email Function
-import smtplib
 def email_sender():
 	file_name = input("What is the name of the file youw ould like to use?")
 	subject = str(input("What do you want the subject of the email to be?  "))
 	body = str(input("What do you want the body of the email to be?  "))
+	app_code = str(input("Put in your appcode. You can get an appcode for your gmail account after enabling 2 factor auth: "))
+	email_address = str(input("Input your the email address you would like to use: "))
 	with open(file_name, 'r') as csv_file:
 		csv_reader = csv.reader(csv_file)
 
@@ -73,12 +57,28 @@ def email_sender():
 
 				msg = f'Subject: {subject}\n\n{body}'
 
-				smtp.sendmail(email_address, "astoshanstuerm1@gmail.com", msg)
-
-
+				smtp.sendmail(email_address, line, msg)
 
 
 #User_Options Config
 
-email_sender()
+options = input("Write premade to use premade .csv file or scrape to enter websites to scrape: ")
 
+if options == "premade":
+	print("You have selected the premade file option. Note that you can only send to 500 different email addresses with this tool(Unless you are going to use multiple accounts). \n	Starting Premade Option....	")
+	email_sender()
+	print(done)
+elif options == "scrape":
+	print("You have selected the scrape option. Note that this option will store a version of chromium.(for python) \n Starting... Scrape Option ")
+	email_scraper()
+	send_emails = input("Would you like to send emails from the new csv file?")
+	if send_emails == "yes":
+		email_sender()
+		print(done)
+	elif send_emails == "no":
+		print(done)
+	else:
+		print(error)
+
+else:
+	print(error)
